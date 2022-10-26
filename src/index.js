04c14d73da7d140d871c5a6a6999856045fddaaf
Sheron Perera <enosh.sheron@gmail.com>
2022-10-26 16:37:54 +0530

    restructure the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import plyr from 'plyr';
+import 'plyr/dist/plyr.css';
+import PropTypes from 'prop-types';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export const PLATFORMS = {
+  YOUTUBE: 'YOUTUBE',
+  VIMEO: 'VIMEO',
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ZotPlayer = (props) => {
+  const [player, setPlayer] = React.useState(null);
+
+  React.useEffect(() => {
+    const playerInstance = new plyr(`#player`);
+    setPlayer(playerInstance);
+
+    return () => {
+      playerInstance.destroy();
+    };
+  }, []);
+
+  const source =
+    props.platform === PLATFORMS.VIMEO
+      ? `https://player.vimeo.com/video/${props.sourceId}`
+      : props.platform === PLATFORMS.YOUTUBE
+      ? `https://www.youtube-nocookie.com/embed/${props.sourceId}`
+      : '';
+
+  return (
+    <div
+      style={{
+        width: props.width ? props.width : '500px',
+        height: props.height ? props.height : '250px',
+      }}
+    >
+      <div id="player">
+        <iframe title={props.sourceId} src={source}></iframe>
+      </div>
+    </div>
+  );
+};
+
+ZotPlayer.propTypes = {
+  platform: PropTypes.string.isRequired,
+  sourceId: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default ZotPlayer;
