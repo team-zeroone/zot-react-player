77506f3ad7fcb509cb8e74e642196c7570431174
Sheron Perera <enosh.sheron@gmail.com>
2022-10-26 16:35:28 +0530

    restructure the project

---
+++ demo/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './App.module.scss';
+import ZotPlayer, { PLATFORMS } from 'zot-player';
+
+const App = () => {
+  // bTqVqk7FSmY --> youtube
+  // 309741585 --> vimeo
+  return (
+    <div className={classes.ParentContainer}>
+      <ZotPlayer
+        platform={PLATFORMS.VIMEO}
+        sourceId="309741585"
+        width="700px"
+        height="350px"
+      />
+    </div>
+  );
+};
+
+export default App;
